38944c1616bd8d7b73540848b6082fc07fed5bed
DevotuOne <devotu.developer@gmail.com>
2020-06-16 22:40:42 +0200

    CLI - Feedback bad input

--- lib/facts/cli.ex
+++ lib/facts/cli.ex
@@ -24,10 +24,19 @@ defmodule Facts.CLI do
     end
   end
 
+
   def process(:help) do
     help_text = """
     Available commands:
-    --help / -h : Display help
+    --help/-h : Display help
+    """
+    IO.puts help_text
+    help_text
+  end
+
+  def process(:no_such_command) do
+    help_text = """
+    No such command. Use --help/-h to display available commands.
     """
     IO.puts help_text
     help_text
