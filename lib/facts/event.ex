98f8c19ddd43b88186a7dd8ce34dc30a9ea3a583
DevotuOne <devotu.developer@gmail.com>
2020-06-30 00:05:41 +0200

    HRC - Event

--- lib/facts/event.ex
+++ lib/facts/event.ex
@@ -3,8 +3,16 @@ defmodule Facts.Event do
 
     alias Facts.Id
     alias Facts.Event
+    alias Facts.HRC
 
-    def new(tags, data) when is_list(tags) do 
+
+    def new(%HRC{subject: nil, predicate: nil} = hrc), do: %Event{id: Id.guid(), tags: {}, data: hrc.details}
+    def new(%HRC{subject: nil} = hrc), do: %Event{id: Id.guid(), tags: {hrc.predicate}, data: hrc.details}
+    def new(%HRC{predicate: nil} = hrc), do: %Event{id: Id.guid(), tags: {hrc.subject}, data: hrc.details}
+    def new(%HRC{} = hrc), do: %Event{id: Id.guid(), tags: {hrc.predicate, hrc.subject}, data: hrc.details}
+
+
+    def new(tags, data) when is_list(tags) do
         %Event{id: Id.guid(), tags: tags, data: data}
     end
 end
--- test/event_test.exs
+++ test/event_test.exs
@@ -2,10 +2,22 @@ defmodule EventTest do
     use ExUnit.Case
 
     alias Facts.Event
+    alias Facts.HRC
 
     test "new event" do
         tags = [:create, :test]
         data = %{data: "testing"}
         assert %Event{id: _guid, tags: tags, data: data} = Event.new(tags, data)
+    end
+
+    test "new event hrc" do
+        hrc = %HRC{predicate: :create, subject: :deck, details: %{name: "Name"}}
+        assert %Event{id: _guid, tags: {:create, :deck}, data: %{name: "Name"}} = Event.new(hrc)
+        hrc = %HRC{subject: :deck, details: %{name: "Name"}}
+        assert %Event{id: _guid, tags: {:deck}, data: %{name: "Name"}} = Event.new(hrc)
+        hrc = %HRC{predicate: :create, details: %{name: "Name"}}
+        assert %Event{id: _guid, tags: {:create}, data: %{name: "Name"}} = Event.new(hrc)
+        hrc = %HRC{details: %{name: "Name"}}
+        assert %Event{id: _guid, tags: {}, data: %{name: "Name"}} = Event.new(hrc)
     end
 end
